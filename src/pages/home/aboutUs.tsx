0edb75706d55ab55e43fac80c4897c8440c6bc9a
Humoyun <ruslonaxmedov44@gmail.com>
2026-04-20 16:21:41 +0500

    add

--- src/pages/home/aboutUs.tsx
+++ src/pages/home/aboutUs.tsx
@@ -1,5 +1,27 @@
-import React from 'react'
+import React from 'react';
 
 export const aboutUs = () => {
-  return <div>aboutUs</div>
+  return (
+    <div className='mx-auto'>
+      <div className='text-2xl font-bold text-red-600'>
+        <div>LinguaPro</div>
+      </div>
+      <div className='flex items-center justify-between'>
+        <ul className='flex gap-4'>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Courses</li>
+          <li>Pricing</li>
+          <li>Contact Us</li>
+          <button>Verify Certificate</button>
+        </ul>
+        <div className='flex gap-4'>
+          <p>Sign In</p>
+          <button className='rounded-2xl bg-red-600 p-2 text-white'>
+            Sign Up
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/pages/home/ourCourses.tsx
+++ src/pages/home/ourCourses.tsx
@@ -1,23 +1,5 @@
 import React from 'react'
 
 export const ourCourses = () => {
-  return <div className='mx-auto'>
-    <div className='text-2xl font-bold text-red-600'>
-      <div>LinguaPro</div>
-    </div>
-    <div className='flex justify-between items-center'>
-      <ul className='flex gap-4'>
-        <li>Home</li>
-        <li>About Us</li>
-        <li>Courses</li>
-        <li>Pricing</li>
-        <li>Contact Us</li>
-        <button>Verify Certificate</button>
-      </ul>
-      <div  className='flex gap-4'>
-        <p>Sign In</p>
-        <button className='bg-red-600 p-2 rounded-2xl text-white'>Sign Up</button>
-      </div>
-    </div>
-  </div>
+  return <div>ourCourses</div>
 }
